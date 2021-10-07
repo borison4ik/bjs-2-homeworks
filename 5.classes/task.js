dec663424ab13a0be0488a15368ceb9a51a46e72
Valentin Borisenko <borison2004@yandex.ru>
2021-10-07 14:00:12 +0700

    resolved first task

---
+++ 5.classes/task.js
@@ -0,0 +1,53 @@
+// Задание №1
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this._state = 100;
+    this.type = null;
+  }
+
+  fix() {
+    this.state = this.state * 1.5;
+  }
+
+  set state(newState) {
+    this._state = newState <= 0 ? 0 : newState >= 100 ? 100 : newState;
+  }
+
+  get state() {
+    return this._state;
+  }
+}
+class Magazine extends PrintEditionItem {
+  constructor(...args) {
+    super(...args);
+    this.type = 'magazine';
+  }
+}
+class Book extends PrintEditionItem {
+  constructor(author, ...args) {
+    super(...args);
+    this.type = 'book';
+    this.author = author;
+  }
+}
+class NovelBook extends Book {
+  constructor(...args) {
+    super(...args);
+    this.type = 'novel';
+  }
+}
+class FantasticBook extends Book {
+  constructor(...args) {
+    super(...args);
+    this.type = 'fantastic';
+  }
+}
+class DetectiveBook extends Book {
+  constructor(...args) {
+    super(...args);
+    this.type = 'detective';
+  }
+}
