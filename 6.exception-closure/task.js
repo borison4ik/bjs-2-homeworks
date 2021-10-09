c597db5bc70fb221d9c2836dc0ab0a35a989f274
Valentin Borisenko <borison2004@yandex.ru>
2021-10-09 17:15:24 +0700

    resolved test for second task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -20,3 +20,56 @@ function validateCount(count) {
 
   return count;
 }
+
+// Задание №2
+
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b <= c || b + c <= a || a + c <= b) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  getPerimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  getArea() {
+    const p = this.getPerimeter() / 2;
+
+    return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+  }
+}
+
+function getTriangle(a, b, c) {
+  let newTriangel = null;
+
+  // не работает так как функция validateCount по условию задачи возвращает ошибку вместо проброса
+  // как быть?
+  // try {
+  //   a = validateCount(a);
+  //   b = validateCount(b);
+  //   c = validateCount(c);
+  // } catch (err) {
+  //   return err;
+  // }
+
+  try {
+    newTriangel = new Triangle(a, b, c);
+  } catch (err) {
+    return {
+      getPerimeter() {
+        return 'Ошибка! Треугольник не существует';
+      },
+
+      getArea() {
+        return 'Ошибка! Треугольник не существует';
+      },
+    };
+  }
+
+  return newTriangel;
+}
