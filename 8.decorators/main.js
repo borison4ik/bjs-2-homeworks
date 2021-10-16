7399114026947d28e2026cb7b4896e87542c590a
Valentin Borisenko <borison2004@yandex.ru>
2021-10-16 21:16:23 +0700

    resolved

--- 8.decorators/main.js
+++ 8.decorators/main.js
@@ -1,20 +1,82 @@
-const addThree = (a, b, c) => (a + b + c); 
+const addThree = (a, b, c) => a + b + c;
 const upgradedAddThree = cachingDecoratorNew(addThree);
-upgradedAddThree(1, 2, 3); 
-upgradedAddThree(1, 2, 3); 
-upgradedAddThree(2, 2, 3); 
-upgradedAddThree(3, 2, 3); 
-upgradedAddThree(4, 2, 3); 
-upgradedAddThree(5, 2, 3); 
-upgradedAddThree(6, 2, 3); 
-upgradedAddThree(1, 2, 3); 
+upgradedAddThree(1, 2, 3);
+upgradedAddThree(1, 2, 3);
+upgradedAddThree(2, 2, 3);
+upgradedAddThree(3, 2, 3);
+upgradedAddThree(4, 2, 3);
+upgradedAddThree(5, 2, 3);
+upgradedAddThree(6, 2, 3);
+upgradedAddThree(1, 2, 3);
 
-const sendSignal = () => console.log('Сигнал послан'); 
-const upgradedSendSignal = debounceDecoratorNew(sendSignal,2000);
-setTimeout(upgradedSendSignal()); 
-setTimeout(upgradedSendSignal(),300); 
-setTimeout(upgradedSendSignal(),900); 
-setTimeout(upgradedSendSignal(),1200);
-setTimeout(upgradedSendSignal(),2300);
-setTimeout(upgradedSendSignal(),4400);
-setTimeout(upgradedSendSignal(),4500);
+const sendSignal = () =>
+  console.log(
+    `Сигнал послан в ${new Date().getMinutes()} : ${new Date().getSeconds()} : ${new Date().getMilliseconds()}`,
+  );
+
+const obj = {
+  text: 'obj text',
+  sendSignal() {
+    return console.log(`Сигнал c this ${this.text} отправлен в ${new Date().getMilliseconds()}`);
+  },
+};
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+setTimeout(upgradedSendSignal);
+setTimeout(() => {
+  console.error('посылаю сигнал через 300мс');
+  upgradedSendSignal();
+}, 300);
+setTimeout(() => {
+  console.error('посылаю сигнал через 900мс');
+  upgradedSendSignal();
+}, 900);
+setTimeout(() => {
+  console.error('посылаю сигнал через 1200мс');
+  upgradedSendSignal();
+}, 1200);
+setTimeout(() => {
+  console.log('посылаю сигнал через 2300мс');
+  upgradedSendSignal();
+}, 2300);
+setTimeout(() => {
+  console.log('посылаю сигнал через 4400мс');
+  upgradedSendSignal();
+}, 4400);
+setTimeout(() => {
+  console.error('посылаю сигнал через 4500мс');
+  upgradedSendSignal();
+}, 4500);
+
+// =============================================================================
+
+const upgradedSendSignal2 = debounceDecorator2(obj.sendSignal, 2000);
+setTimeout(() => upgradedSendSignal2.call(obj), 5000);
+setTimeout(() => {
+  console.error('посылаю сигнал через 5300мс');
+  upgradedSendSignal2.call(obj);
+}, 5300);
+setTimeout(() => {
+  console.error('посылаю сигнал через 5900мс');
+  upgradedSendSignal2.call(obj);
+}, 5900);
+setTimeout(() => {
+  console.error('посылаю сигнал через 6200мс');
+  upgradedSendSignal2.call(obj);
+}, 6200);
+setTimeout(() => {
+  console.log('посылаю сигнал через 7300мс');
+  upgradedSendSignal2.call(obj);
+}, 7300);
+setTimeout(() => {
+  console.log('посылаю сигнал через 9400мс');
+  upgradedSendSignal2.call(obj);
+}, 9400);
+setTimeout(() => {
+  console.error('посылаю сигнал через 9500мс');
+  upgradedSendSignal2.call(obj);
+}, 9500);
+
+setTimeout(
+  () => console.log(`количество вызовов upgradedSendSignal2: ${upgradedSendSignal2.count}`),
+  9600,
+);
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,53 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  return function (...args) {
+    const hash = args.join();
+    const cacheItem = cache.find((item) => item.hash === hash);
+
+    if (cacheItem) {
+      return `Из кэша: ${cacheItem.result}`;
+    } else {
+      const result = func.call(this, ...args);
+      if (cache.length == 5) {
+        cache = cache.slice(1);
+        cache.push({ hash, result });
+      } else {
+        cache.push({ hash, result });
+      }
+      return `Вычисляем: ${result}`;
+    }
+  };
 }
 
+function debounceDecoratorNew(func, ms) {
+  let isDelayed = false;
+  // можно сразу вернуть не именованную функцию
+  return function (...args) {
+    if (!isDelayed) {
+      func.call(this, ...args);
+      isDelayed = true;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+      setTimeout(() => {
+        isDelayed = false;
+      }, ms);
+    }
+  };
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let isDelayed = false;
+  let count = 0;
+  // здесь пришлось вернуть именованную функцию чтобы обратиться к ней по имени и создать новое свойство
+  return function wrapper(...args) {
+    wrapper.count = ++count;
+    if (!isDelayed) {
+      func.call(this, ...args);
+      isDelayed = true;
+
+      setTimeout(() => {
+        isDelayed = false;
+      }, ms);
+    }
+  };
 }
