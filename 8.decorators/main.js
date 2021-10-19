a12e0d44b6ddbbb51315ae2ebb3ce2c29f8b01c3
Valentin Borisenko <borison2004@yandex.ru>
2021-10-19 23:52:12 +0700

    debounce fixed

--- 8.decorators/main.js
+++ 8.decorators/main.js
@@ -20,63 +20,58 @@ const obj = {
     return console.log(`Сигнал c this ${this.text} отправлен в ${new Date().getMilliseconds()}`);
   },
 };
-const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
-setTimeout(upgradedSendSignal);
-setTimeout(() => {
-  console.error('посылаю сигнал через 300мс');
-  upgradedSendSignal();
-}, 300);
-setTimeout(() => {
-  console.error('посылаю сигнал через 900мс');
-  upgradedSendSignal();
-}, 900);
-setTimeout(() => {
-  console.error('посылаю сигнал через 1200мс');
-  upgradedSendSignal();
-}, 1200);
-setTimeout(() => {
-  console.log('посылаю сигнал через 2300мс');
-  upgradedSendSignal();
-}, 2300);
-setTimeout(() => {
-  console.log('посылаю сигнал через 4400мс');
-  upgradedSendSignal();
-}, 4400);
-setTimeout(() => {
-  console.error('посылаю сигнал через 4500мс');
-  upgradedSendSignal();
-}, 4500);
+// const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+// setTimeout(upgradedSendSignal);
+// setTimeout(() => {
+//   console.error('посылаю сигнал через 300мс');
+//   upgradedSendSignal();
+// }, 300);
+// setTimeout(() => {
+//   console.error('посылаю сигнал через 900мс');
+//   upgradedSendSignal();
+// }, 900);
+// setTimeout(() => {
+//   console.error('посылаю сигнал через 1200мс');
+//   upgradedSendSignal();
+// }, 1200);
+// setTimeout(() => {
+//   console.error('посылаю сигнал через 2300мс');
+//   upgradedSendSignal();
+// }, 2300);
+// setTimeout(() => {
+//   console.log('посылаю сигнал через 4400мс');
+//   upgradedSendSignal();
+// }, 4400);
+// setTimeout(() => {
+//   console.error('посылаю сигнал через 4500мс');
+//   upgradedSendSignal();
+// }, 4500);
 
 // =============================================================================
 
 const upgradedSendSignal2 = debounceDecorator2(obj.sendSignal, 2000);
-setTimeout(() => upgradedSendSignal2.call(obj), 5000);
+setTimeout(() => upgradedSendSignal2.call(obj));
 setTimeout(() => {
-  console.error('посылаю сигнал через 5300мс');
+  console.error('посылаю сигнал через 300мс');
   upgradedSendSignal2.call(obj);
-}, 5300);
+}, 300);
 setTimeout(() => {
-  console.error('посылаю сигнал через 5900мс');
+  console.error('посылаю сигнал через 900мс');
   upgradedSendSignal2.call(obj);
-}, 5900);
+}, 900);
 setTimeout(() => {
-  console.error('посылаю сигнал через 6200мс');
+  console.error('посылаю сигнал через 1200мс');
   upgradedSendSignal2.call(obj);
-}, 6200);
+}, 1200);
 setTimeout(() => {
-  console.log('посылаю сигнал через 7300мс');
+  console.error('посылаю сигнал через 2300мс');
   upgradedSendSignal2.call(obj);
-}, 7300);
+}, 2300);
 setTimeout(() => {
-  console.log('посылаю сигнал через 9400мс');
+  console.log('посылаю сигнал через 4400мс');
   upgradedSendSignal2.call(obj);
-}, 9400);
+}, 4400);
 setTimeout(() => {
-  console.error('посылаю сигнал через 9500мс');
+  console.error('посылаю сигнал через 4500мс');
   upgradedSendSignal2.call(obj);
-}, 9500);
-
-setTimeout(
-  () => console.log(`количество вызовов upgradedSendSignal2: ${upgradedSendSignal2.count}`),
-  9600,
-);
+}, 4500);
